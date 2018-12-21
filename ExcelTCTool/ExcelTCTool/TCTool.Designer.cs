9a5991195fc0a4fc5381cd1ed1268bc4f8ccbd5d
ZcyRyan <37356410+ZcyRyan@users.noreply.github.com>
2018-12-21 10:52:16 +0800

    Excel TC Tool
    Excel Add-in to control auto line number

---
+++ ExcelTCTool/ExcelTCTool/TCTool.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ExcelTCTool
+{
+    partial class TCTool : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public TCTool()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grp1 = this.Factory.CreateRibbonGroup();
+            this.btnSetTC = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.grp1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.grp1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // grp1
+            // 
+            this.grp1.Items.Add(this.btnSetTC);
+            this.grp1.Items.Add(this.button1);
+            this.grp1.Label = "ZcyTool";
+            this.grp1.Name = "grp1";
+            // 
+            // btnSetTC
+            // 
+            this.btnSetTC.Label = "SetTC";
+            this.btnSetTC.Name = "btnSetTC";
+            this.btnSetTC.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSetTC_Click);
+            // 
+            // button1
+            // 
+            this.button1.Label = "SetCellNum";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // TCTool
+            // 
+            this.Name = "TCTool";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.TCTool_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grp1.ResumeLayout(false);
+            this.grp1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grp1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnSetTC;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal TCTool TCTool
+        {
+            get { return this.GetRibbon<TCTool>(); }
+        }
+    }
+}
---
+++ ExcelTCTool/ExcelTCTool/TCTool.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+
+namespace ExcelTCTool
+{
+    public partial class TCTool
+    {
+
+        private const string TC_CASE_HEADERKEY = "WNV3_";
+        private const int TC_CASENO_COLUMN = 1;
+        private const int NON_COLOR = -4142;
+        private const int FORMAT_START = 7;
+
+        private Worksheet sheet;
+
+        private void TCTool_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void btnSetTC_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            try
+            {
+                sheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+                int MaxRow = ((Range)(sheet.Cells[sheet.Rows.Count, TC_CASENO_COLUMN]))
+                     .End[Microsoft.Office.Interop.Excel.XlDirection.xlUp].Row;
+                int CaseNo = 1;
+                string caseHeader = string.Empty;
+                bool BsetFirst = false;
+                int columnCount = GetColumn();
+
+                for (int i = 3; i <= MaxRow; i++)
+                {
+                    Range rg = sheet.Cells[i, TC_CASENO_COLUMN];
+
+                    if (rg.Interior.ColorIndex != NON_COLOR)
+                    {
+                        Range headerRange = sheet.Range[sheet.Cells[i, 1], sheet.Cells[i, columnCount]];
+                        headerRange.Borders[XlBordersIndex.xlEdgeTop].Weight = XlBorderWeight.xlMedium;
+                    }
+
+                    else
+                    {
+
+                        if (!BsetFirst)
+                        {
+                            string val = rg.Value;
+                            if (val.Trim().StartsWith(TC_CASE_HEADERKEY))
+                            {
+                                CaseNo = Convert.ToInt16(val.Substring(val.Length - 3, 3));
+                                caseHeader = val.Substring(0, val.Length - 3);
+                            }
+                            BsetFirst = true;     
+                        }
+
+                        sheet.Cells[i, TC_CASENO_COLUMN].value = caseHeader
+                            + CaseNo.ToString().PadLeft(3, '0');
+                        CaseNo++;
+                        sheet.Rows[i].AutoFit();
+                    }
+
+                    CopyCellFormat(sheet.Cells[i, 1], sheet.Range[sheet.Cells[i, FORMAT_START], sheet.Cells[i, columnCount]]);
+                }
+
+                sheet.Cells[MaxRow, 1].Select();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private int GetColumn()
+        {
+            int ret;
+            sheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+            for (int i = 1; ; i++ )
+            {
+                Range rg = sheet.Cells[1, i];
+                if (rg.Interior.ColorIndex != NON_COLOR)
+                {
+                    continue;
+                }
+                else
+                {
+                    ret = i - 1;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+
+        private void CopyCellFormat(Range origin, Range target)
+        {
+            origin.Copy(Type.Missing);
+            target.PasteSpecial(XlPasteType.xlPasteFormats);
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            sheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+            string value = string.Empty;
+            int startIndex = 1;
+            string header = string.Empty;
+            string content = string.Empty;
+            string result = string.Empty;
+            try
+            {
+                Range rg = Globals.ThisAddIn.Application.ActiveCell;
+                if (rg != null)
+                {
+                    value = rg.Value;
+                    var groupvalue = value.Split('\n');
+                    startIndex = Convert.ToInt16(groupvalue[0].Substring(0, 1));
+                    foreach (string v in groupvalue)
+                    {
+                        if (!string.IsNullOrEmpty(v.Trim()))
+                        {
+                            int pos = v.IndexOf(".");
+                            if ((pos >= 0) && (pos<=4))
+                            {
+                                content = v.Substring(pos + 1);
+                            }
+                            else
+                            {
+                                content = v.TrimStart();
+                            }
+                            header = startIndex.ToString() + ". ";
+                            result += header + content.Trim() + '\n';
+                            startIndex++;
+                        }
+                    }
+                }
+
+                rg.Value = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}

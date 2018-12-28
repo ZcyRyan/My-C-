ad41ab0ce89ff5a04326935761938f527aca643c
ZcyRyan <37356410+ZcyRyan@users.noreply.github.com>
2018-12-28 17:08:07 +0800

    Simulator Debuger

---
+++ SimulatorDebugTool/SimulatorDebugTool/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace SimulatorDebugTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDebug = new System.Windows.Forms.Button();
+            this.btnNotDebug = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDebug
+            // 
+            this.btnDebug.Location = new System.Drawing.Point(54, 100);
+            this.btnDebug.Name = "btnDebug";
+            this.btnDebug.Size = new System.Drawing.Size(75, 23);
+            this.btnDebug.TabIndex = 0;
+            this.btnDebug.Text = "Debug";
+            this.btnDebug.UseVisualStyleBackColor = true;
+            this.btnDebug.Click += new System.EventHandler(this.btnDebug_Click);
+            // 
+            // btnNotDebug
+            // 
+            this.btnNotDebug.Location = new System.Drawing.Point(146, 100);
+            this.btnNotDebug.Name = "btnNotDebug";
+            this.btnNotDebug.Size = new System.Drawing.Size(75, 23);
+            this.btnNotDebug.TabIndex = 1;
+            this.btnNotDebug.Text = "Not Debug";
+            this.btnNotDebug.UseVisualStyleBackColor = true;
+            this.btnNotDebug.Click += new System.EventHandler(this.btnNotDebug_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bin Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(77, 48);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(421, 21);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPath_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 143);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNotDebug);
+            this.Controls.Add(this.btnDebug);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SimulatorDebugSetting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Button btnNotDebug;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+    }
+}
+
---
+++ SimulatorDebugTool/SimulatorDebugTool/Form1.cs
@@ -0,0 +1,176 @@
+﻿using SimulatorDebugTool.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimulatorDebugTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPath_DragEnter(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData("FileNameW") as string[];
+            this.txtPath.Text = data.FirstOrDefault();
+        }
+
+        private void btnDebug_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.Trim();
+            string content = string.Empty;
+            if (Directory.Exists(path))
+            {
+                string dest1 = Directory.GetParent(path).FullName + @"\Source\WindLDR\MainForm.WNV.cs";
+                string dest2 = Directory.GetParent(path).FullName + @"\Source\WindowsSimulator\SimulatorForm\SimulatorFormHandler.cs";
+                using (StreamReader rd = new StreamReader(dest1))
+                {
+                    content = rd.ReadToEnd();
+                }
+                int index = content.IndexOf("#define SIMULATORDEBUG");
+                if (index >= 0)
+                {
+                    string comment = content.Substring(index - 2, 2);
+                    if (comment == @"//")
+                    {
+                        content = content.Substring(0, index - 2) + content.Substring(index);
+                        using (StreamWriter wr = new StreamWriter(dest1, false, Encoding.UTF8))
+                        {
+                            wr.Write(content);
+                            wr.Flush();
+                        }
+                    }
+                }
+
+
+                using (StreamReader rd = new StreamReader(dest2))
+                {
+                    content = rd.ReadToEnd();
+                }
+                index = content.IndexOf("#define SIMULATORDEBUG");
+                if (index >= 0)
+                {
+                    string comment = content.Substring(index - 2, 2);
+                    if (comment == @"//")
+                    {
+                        content = content.Substring(0, index - 2) + content.Substring(index);
+                        using (StreamWriter wr = new StreamWriter(dest2, false, Encoding.UTF8))
+                        {
+                            wr.Write(content);
+                            wr.Flush();
+                        }
+                    }
+                }
+                MessageBox.Show("Over!");
+                string defaultPath = Environment.CurrentDirectory + @"\DefaultPath.txt";
+                if (!File.Exists(defaultPath))
+                {
+                    using (var fileStream = File.CreateText(defaultPath))
+                    {
+                        fileStream.Write(path);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter wr = new StreamWriter(defaultPath, false))
+                    {
+                        wr.Write(path);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Path Error!");
+            }
+        }
+
+        private void btnNotDebug_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.Trim();
+            string content = string.Empty;
+            if (Directory.Exists(path))
+            {
+                string dest1 = Directory.GetParent(path).FullName + @"\Source\WindLDR\MainForm.WNV.cs";
+                string dest2 = Directory.GetParent(path).FullName + @"\Source\WindowsSimulator\SimulatorForm\SimulatorFormHandler.cs";
+                using (StreamReader rd = new StreamReader(dest1))
+                {
+                    content = rd.ReadToEnd();
+                }
+                int index = content.IndexOf("#define SIMULATORDEBUG");
+                if (index >= 0)
+                {
+                    string comment = content.Substring(index - 2, 2);
+                    if (comment != @"//")
+                    {
+                        content = content.Substring(0, index) + @"//" + content.Substring(index);
+                        using (StreamWriter wr = new StreamWriter(dest1, false, Encoding.UTF8))
+                        {
+                            wr.Write(content);
+                            wr.Flush();
+                        }
+                    }
+                }
+
+                using (StreamReader rd = new StreamReader(dest2))
+                {
+                    content = rd.ReadToEnd();
+                }
+                index = content.IndexOf("#define SIMULATORDEBUG");
+                if (index >= 0)
+                {
+                    string comment = content.Substring(index - 2, 2);
+                    if (comment != @"//")
+                    {
+                        content = content.Substring(0, index) + @"//" + content.Substring(index);
+                        using (StreamWriter wr = new StreamWriter(dest2, false, Encoding.UTF8))
+                        {
+                            wr.Write(content);
+                            wr.Flush();
+                        }
+                    }
+                }
+                MessageBox.Show("Over!");
+                string defaultPath = Environment.CurrentDirectory + @"\DefaultPath.txt";
+                if (!File.Exists(defaultPath))
+                {
+                    using (var fileStream = File.CreateText(defaultPath))
+                    {
+                        fileStream.Write(path);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter wr = new StreamWriter(defaultPath, false))
+                    {
+                        wr.Write(path);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Path Error!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string defaultPath = Environment.CurrentDirectory + @"\DefaultPath.txt";
+            if (File.Exists(defaultPath))
+            {
+                using (StreamReader rd = new StreamReader(defaultPath))
+                {
+                    txtPath.Text = rd.ReadLine();
+                }
+            }
+        }
+    }
+}

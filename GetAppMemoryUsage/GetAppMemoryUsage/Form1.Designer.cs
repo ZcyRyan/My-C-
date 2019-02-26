37790d1e1aa14cac4c62b6b4472e7882b16fb5ad
ZcyRyan <37356410+ZcyRyan@users.noreply.github.com>
2019-02-26 10:29:04 +0800

    Commit get memory
    memory check

---
+++ GetAppMemoryUsage/GetAppMemoryUsage/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GetAppMemoryUsage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(45, 66);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(363, 179);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(134, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(202, 21);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "WINDOI-NV4";
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(353, 27);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(55, 23);
+            this.btnInfo.TabIndex = 2;
+            this.btnInfo.Text = "Info.";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Process Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 283);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GetAppMemoryUsage/GetAppMemoryUsage/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetAppMemoryUsage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            //var p = Process.GetProcessesByName(txtName.Text).FirstOrDefault();
+            //StringBuilder bd = new StringBuilder();
+            //bd.Append(p.WorkingSet.ToString());
+            //txtMessage.Text = bd.ToString();
+
+            StringBuilder bd = new StringBuilder();
+            var counter = new PerformanceCounter("Process", "Working Set - Private", txtName.Text);
+            bd.Append(string.Format("{0}K", counter.RawValue / 1024));
+            txtMessage.Text = bd.ToString();
+        }
+    }
+}
